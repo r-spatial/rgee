4ed1efd2ef301452ff08484de1cc375f855c5002
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2019-08-23 14:29:19 -0500

    ee_check_utils_exist.py final version
    > PEP-8 style, documented and tested.

--- inst/python/ee_check_utils_exist.py
+++ inst/python/ee_check_utils_exist.py
@@ -1,157 +1,197 @@
-#python Standard Libraries
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""Check Python modules
+
+>>> ee_check_py_webbrowser()
+>>> ee_check_py_platform()
+>>> ee_check_py_request()
+>>> ee_check_py_zipfile()
+>>> ee_check_py_tarfile()
+>>> ee_check_py_subprocess()
+>>> ee_check_py_retrying()
+>>> ee_check_py_ast()
+>>> ee_check_py_sys()
+>>> ee_check_py_os()
+>>> ee_check_py_time()
+>>> ee_check_py_json()
+
+>>> ee_check_py_ee()
+>>> ee_check_py_selenium()
+>>> ee_check_py_bs4()
+>>> ee_check_py_pysmartDL()
+>>> ee_check_py_requests_toolbelt()
+
+The functions will be used since for R/ee_check.R
+"""
 def ee_check_py_webbrowser():
-  try:
-    import webbrowser
     try:
-      return webbrowser.__version__
+        import webbrowser
+        try:
+            return webbrowser.__version__
+        except:
+            return True
     except:
-      return True
-  except:
-    return False
+        return False
+
 
 def ee_check_py_platform():
-  try:
-    import platform
     try:
-      return platform.__version__
+        import platform
+        try:
+            return platform.__version__
+        except:
+            return True
     except:
-      return True
-  except:
-    return False
+        return False
+
 
 def ee_check_py_request():
-  try:
-    import request
     try:
-      return request.__version__
+        import request
+        try:
+            return request.__version__
+        except:
+            return True
     except:
-      return True
-  except:
-    return False
+        return False
+
 
 def ee_check_py_zipfile():
-  try:
-    import zipfile
     try:
-      return zipfile.__version__
+        import zipfile
+        try:
+            return zipfile.__version__
+        except:
+            return True
     except:
-      return True
-  except:
-    return False
+        return False
+
 
 def ee_check_py_tarfile():
-  try:
-    import tarfile
     try:
-      return tarfile.__version__
+        import tarfile
+        try:
+            return tarfile.__version__
+        except:
+            return True
     except:
-      return True
-  except:
-    return False
+        return False
+
 
 def ee_check_py_subprocess():
-  try:
-    import subprocess
     try:
-      return subprocess.__version__
+        import subprocess
+        try:
+            return subprocess.__version__
+        except:
+            return True
     except:
-      return True
-  except:
-    return False
+        return False
+
 
 def ee_check_py_retrying():
-  try:
-    import retrying
     try:
-      return retrying.__version__
+        import retrying
+        try:
+            return retrying.__version__
+        except:
+            return True
     except:
-      return True
-  except:
-    return False
+        return False
+
 
 def ee_check_py_ast():
-  try:
-    import ast
     try:
-      return ast.__version__
+        import ast
+        try:
+            return ast.__version__
+        except:
+            return True
     except:
-      return True
-  except:
-    return False
+        return False
+
 
 def ee_check_py_sys():
-  try:
-    import sys
     try:
-      return sys.__version__
+        import sys
+        try:
+            return sys.__version__
+        except:
+            return True
     except:
-      return True
-  except:
-    return False
+        return False
+
 
 def ee_check_py_os():
-  try:
-    import os
     try:
-      return os.__version__
+        import os
+        try:
+            return os.__version__
+        except:
+            return True
     except:
-      return True
-  except:
-    return False
+        return False
+
 
 def ee_check_py_time():
-  try:
-    import time
     try:
-      return time.__version__
+        import time
+        try:
+            return time.__version__
+        except:
+            return True
     except:
-      return True
-  except:
-    return False
+        return False
+
 
 def ee_check_py_json():
-  try:
-    import json
     try:
-      return json.__version__
+        import json
+        try:
+            return json.__version__
+        except:
+            return True
     except:
-      return True
-  except:
-    return False
+        return False
 
-#Third-party package
+
+# Third-party package
 def ee_check_py_ee():
-  try:
-    import ee
-    return ee.__version__
-  except:
-    return False
+    try:
+        import ee
+        return ee.__version__
+    except:
+        return False
+
 
 def ee_check_py_selenium():
-  try:
-    import selenium
-    return selenium.__version__
-  except:
-    return False
+    try:
+        import selenium
+        return selenium.__version__
+    except:
+        return False
+
 
 def ee_check_py_bs4():
-  try:
-    import bs4
-    return bs4.__version__
-  except:
-    return False
+    try:
+        import bs4
+        return bs4.__version__
+    except:
+        return False
+
 
 def ee_check_py_pysmartDL():
-  try:
-    import pySmartDL
-    return pySmartDL.__version__
-  except:
-    return False
+    try:
+        import pySmartDL
+        return pySmartDL.__version__
+    except:
+        return False
+
 
 def ee_check_py_requests_toolbelt():
-  try:
-    import requests_toolbelt
-    return requests_toolbelt.__version__
-  except:
-    return False
-
+    try:
+        import requests_toolbelt
+        return requests_toolbelt.__version__
+    except:
+        return False
