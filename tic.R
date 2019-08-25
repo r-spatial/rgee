be82eba543ae2312ce632243444f5dcfc4449aef
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2019-08-25 01:57:14 -0500

    travis without python testing

--- tic.R
+++ tic.R
@@ -4,4 +4,13 @@ if (ci_on_travis()) {
   do_pkgdown()
 }
 
-
+get_stage("script") %>%
+  add_code_step({
+    install.packages("rjson")
+    library(rjson)
+    key <- Sys.getenv("EE_CREDENTIALS")
+    json_key <- toJSON(list(refresh_token = key))
+    ee_dirname <- path.expand("~/.config/earthengine")
+    dir.create(ee_dirname)
+    write(json_key, sprintf("%s/credentials",ee_dirname))
+  })
