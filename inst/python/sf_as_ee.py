ed44dc53cb3ea2ccde59847943cc1b669edceb44
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2019-09-05 19:54:58 -0500

    Issue #12: sf_as_ee
    Fixed by adding regex to the Python code

--- inst/python/sf_as_ee.py
+++ inst/python/sf_as_ee.py
@@ -11,18 +11,19 @@ import ee
 
 These functions are using in R/sf_as_ee.R
 """
-
+import re
 
 def sf_as_ee_py(x):
-    # sf to ee
+    x = re.sub(r':null', ':None', x)
     return ee.FeatureCollection(eval(x)['features'])
 
 
 def sfc_as_ee_py(x):
+    x = re.sub(r':null', ':None', x)
     # sfc to ee
     return ee.Geometry(eval(x))
 
-
 def sfg_as_ee_py(x):
+    x = re.sub(r':null', ':None', x)
     # sfg to ee
     return ee.Geometry(eval(x))
