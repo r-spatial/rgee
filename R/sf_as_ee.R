c76c629edb08d2b663b3ea78798152d23c30c0ca
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2019-09-06 21:06:18 -0500

    sf_as_ee
    Tested version

--- R/sf_as_ee.R
+++ R/sf_as_ee.R
@@ -1,64 +1,48 @@
-#' Convert R spatial object to an Google Earth Engine
+#' Convert an sf object to EE object
 #'
 #' @name sf_as_ee
-#' @param x object to be converted into.
+#' @param x sf object to be converted into a EE object.
 #' @param check_ring_dir logical. see \link[sf]{st_read}.
-#' @param ... \link[sf]{st_read} arguments might be passed.
-#' @importFrom sf st_as_sf read_sf write_sf
-#' @importFrom geojsonio geojson_json geojson_list
-#' @importFrom sf st_transform
+#' @param ... \link[sf]{st_read} arguments might be included.
+#' @importFrom sf read_sf
+#' @importFrom geojsonio geojson_json
 #' @details
-#' \code{sf_as_ee} try to transform R objects (sf) into a GeoJSON format. By the time a user sends an HTTP
-#' request to the Earth Engine Web REST APIs (by, e.g. using *$getInfo()), the GeoJSON will be
-#' encrusted to the message. Therefore, it is expected that large spatial objects (>500Kb) cause
-#' bottlenecks and plodding connections. See
+#' The conversion from sf to EE is a two-step process. Firstly, \code{sf_as_ee} transform
+#' sf objects into a GeoJSON format using \link[geojsonio]{geojson_json}. Secondly, the GeoJSON
+#' generated will be encrusted in an HTTP request using the server-side objects (ee$Geometry$*).
+#' If the sf object is large spatial objects (>1Mb) it is expected that cause bottlenecks and
+#' plodding connections. See
 #' \href{https://developers.google.com/earth-engine/client_server#client-and-server-functions}{Client vs Server}
-#' documentation for details. For leading with very large spatial objects, is a good practice firstly
-#' importing it to the GEE assets. See \code{\link[rgee]{ee_upload_toasset}} for details.
-#'
+#' documentation for details. For leading with very large spatial objects, is recommended firstly
+#' importing it to the GEE assets. See \link[rgee]{ee_upload} for creating uploading pipelines.
 #' @examples
-#' \dontrun{
 #' library(rgee)
-#' ee_initialize()
+#' library(sf)
+#' ee_Initialize()
 #'
-#' # character
-#' x <- system.file("shape/nc.shp", package="sf")
-#' ee$FeatureCollection(ee_as_ee(x),proj="EPSG:4326")
-#'
-#' # matrix
-#' ee_df <- matrix(rnorm(100),ncol = 5) %>% as.data.frame()
-#' ee_df <- ee_as_ee(ee_df,c("V1","V2"))
-#'
-#' ee_df$geometry()
 #' # sf
-#' x <- read_sf(system.file("shape/nc.shp", package="sf"))
-#' st_crs
-#' ee_as_ee(x)
-#'
-#' #sfc
-#' x <- read_sf(system.file("shape/nc.shp", package="sf"))$geometry
-#' ee_as_ee(x)
-#'
-#' #sfg
-#' x <- read_sf(system.file("shape/nc.shp", package="sf"))$geometry[[1]]
-#' ee_as_ee(x)
-#'
-#' #sp
-#' x <- shapefile(system.file("shape/nc.shp", package="sf"))
-#' ee_as_ee(x)
-#' }
+#' x <- read_sf(system.file("shape/nc.shp", package = "sf")) %>% st_transform(4326)
+#' ee_x <- sf_as_ee(x, check_ring_dir = TRUE)
+#' ee_map(ee_x)
+#' # sfc
+#' x <- read_sf(system.file("shape/nc.shp", package = "sf"))$geometry %>% st_transform(4326)
+#' ee_x <- sf_as_ee(x, check_ring_dir = TRUE)
+#' ee_map(ee_x)
+#' # sfg
+#' x <- read_sf(system.file("shape/nc.shp", package = "sf"))$geometry[[1]]
+#' ee_x <- sf_as_ee(x, check_ring_dir = TRUE)
+#' ee_map(ee_x)
 #' @export
 sf_as_ee <- function(x, check_ring_dir) UseMethod("sf_as_ee")
 
 #' @rdname sf_as_ee
-#' @importFrom sf st_read
 #' @export
-sf_as_ee.character <- function(x,check_ring_dir = FALSE, ...) {
+sf_as_ee.character <- function(x, check_ring_dir = FALSE, ...) {
   oauth_func_path <- system.file("python/sf_as_ee.py", package = "rgee")
   sf_as_ee <- ee_source_python(oauth_func_path)
   eex <- st_read(x, check_ring_dir = check_ring_dir, quiet = TRUE, ...)
-  if (!st_crs(eex)$epsg == 4326){ #deprecated
-    eex <- st_transform(eex, 4326)
+  if (!st_crs(eex)$epsg == 4326) {
+    stop("The x EPSG needs to be 4326, use sf::st_transform.")
   }
   geojson_list <- geojson_json(eex)
   sf_as_ee$sf_as_ee_py(geojson_list)
@@ -67,10 +51,12 @@ sf_as_ee.character <- function(x,check_ring_dir = FALSE, ...) {
 #' @rdname sf_as_ee
 #' @export
 sf_as_ee.sf <- function(x, check_ring_dir = FALSE) {
-  x <- st_sf(x,check_ring_dir = TRUE)
+  x <- st_sf(x, check_ring_dir = TRUE)
   oauth_func_path <- system.file("python/sf_as_ee.py", package = "rgee")
   sf_as_ee <- ee_source_python(oauth_func_path)
-  if (!st_crs(x)$epsg == 4326) x <- st_transform(x, 4326) #deprecated
+  if (!st_crs(x)$epsg == 4326) {
+    stop("The x EPSG needs to be 4326, use sf::st_transform.")
+  }
   geojson_list <- geojson_json(x)
   sf_as_ee$sf_as_ee_py(geojson_list)
 }
@@ -81,7 +67,9 @@ sf_as_ee.sfc <- function(x, check_ring_dir = FALSE) {
   x <- st_sfc(x, check_ring_dir = check_ring_dir)
   oauth_func_path <- system.file("python/sf_as_ee.py", package = "rgee")
   sf_as_ee <- ee_source_python(oauth_func_path)
-  if (!st_crs(x)$epsg == 4326) x <- st_transform(x, 4326) #deprecated
+  if (!st_crs(x)$epsg == 4326) {
+    stop("The x EPSG needs to be 4326, use sf::st_transform.")
+  }
   geojson_list <- geojson_json(x)
   sf_as_ee$sfc_as_ee_py(geojson_list)
 }
@@ -89,6 +77,7 @@ sf_as_ee.sfc <- function(x, check_ring_dir = FALSE) {
 #' @rdname sf_as_ee
 #' @export
 sf_as_ee.sfg <- function(x, check_ring_dir = FALSE) {
+  message("EPSG:4326 assigned as an coordinate reference system")
   x <- st_sfc(x, check_ring_dir = check_ring_dir)
   oauth_func_path <- system.file("python/sf_as_ee.py", package = "rgee")
   sf_as_ee <- ee_source_python(oauth_func_path)
---
+++ tests/testthat/test_rgee_sf_as_ee.R
@@ -0,0 +1,37 @@
+library(testthat)
+library(rgee)
+library(raster)
+ee_Initialize()
+
+filename <- system.file("external/lux.shp", package="raster")
+context("rgee: sf_as_ee test")
+
+test_that("character",{
+  p <- filename %>%
+    sf_as_ee(check_ring_dir = TRUE)
+  expect_is(p, "ee.featurecollection.FeatureCollection")
+})
+
+test_that("sf",{
+  p <- shapefile(filename) %>%
+    st_as_sf() %>%
+    sf_as_ee(check_ring_dir = TRUE)
+  expect_is(p, "ee.featurecollection.FeatureCollection")
+})
+
+test_that("sfc",{
+  p <- shapefile(filename) %>%
+    st_as_sf() %>%
+    st_geometry() %>%
+    sf_as_ee(check_ring_dir = TRUE)
+  expect_is(p, "ee.geometry.Geometry")
+})
+
+test_that("sfg",{
+  p <- shapefile(filename) %>%
+    st_as_sf() %>%
+    st_geometry() %>%
+    '[['(1) %>%
+    sf_as_ee(check_ring_dir = TRUE)
+  expect_is(p, "ee.geometry.Geometry")
+})
