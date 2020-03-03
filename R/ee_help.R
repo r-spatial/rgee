be29acc68deb27df18ec47c36861ae1781c762f8
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2020-03-02 23:22:33 -0500

    ee_help in a rstudio env

--- R/ee_help.R
+++ R/ee_help.R
@@ -4,8 +4,10 @@
 #' @examples
 #' \dontrun{
 #' ee_Initialize()
-#' ee_help(eeobject = ee$Image()$geometry()$centroid)
-#' ee_help(eeobject = ee$Image()$geometry)
+#' ee_reattach()
+#' ee$Image()$geometry()$centroid %>% ee_help
+#' ee$Image()$geometry %>% ee_help
+#' ee$Image %>% ee_help
 #' ee_help(eeobject = ee$ImageCollection(list())$size)
 #' }
 ee_help <- function(eeobject) {
@@ -25,15 +27,15 @@ ee_help <- function(eeobject) {
   if (rstudioapi::isAvailable()) {
     writeLines(
       text = c(ee_html_head_rstudio(doc_to_display$qualified_name),
-               ee_html_title_simple(doc_to_display$description),
-               ee_html_description_simple(doc_to_display$description),
-               ee_html_usage_simple(doc_to_display),
-               ee_html_arguments_simple(doc_to_display$parameters),
-               ee_html_details_simple(doc_to_display$details),
-               ee_html_returns_simple(doc_to_display$returns)),
+               ee_html_title_rstudio(doc_to_display$description),
+               ee_html_description_rstudio(doc_to_display$description),
+               ee_html_usage_rstudio(doc_to_display),
+               ee_html_arguments_rstudio(doc_to_display$parameters),
+               ee_html_details_rstudio(doc_to_display$details),
+               ee_html_returns_rstudio(doc_to_display$returns)),
       con =  fileConn
     )
-    close(fileConn)
+    #close(fileConn)
     rstudioapi::viewer(temp_file)
   } else {
     writeLines(
@@ -84,9 +86,16 @@ ee_html_head_rstudio <- function(fun_name) {
   gsub('td_style', td_style, message_norgee)
 }
 
+
 #' Create main title - R Documentation Simple
 #' @noRd
 ee_html_title_simple <- function(title){
+  sprintf('<h2 style="%s">%s</h2>',ee_css_h2_simple(),title)
+}
+
+#' Create main title - R Documentation Rstudio
+#' @noRd
+ee_html_title_rstudio <- function(title){
   sprintf('<h2 style="%s">%s</h2>',ee_css_h2_rstudio(),title)
 }
 
@@ -114,9 +123,11 @@ ee_html_description_rstudio <- function(descrp){
     ' Documentation obtained from this <a href="%s">link</a>.',
     api_ref
   )
+  p_style <- 'font-family: sans-serif; font-size: 10pt;'
   sprintf(
-    '<h3 style="%s">Description</h3>\n\n<p>%s%s<p>',
+    '<h3 style="%s">Description</h3><p style="%s">%s%s<p>',
     ee_css_h3_rstudio(),
+    p_style,
     descrp,
     gee_message
   )
@@ -135,10 +146,26 @@ ee_html_usage_simple <- function(pydoc){
   sprintf('<h3 style="%s">Usage</h3><p style = "%s">%s</p>',ee_css_h3_simple(),ee_css_pre_simple(),to_display)
 }
 
+#' Create usage - R Documentation Simple
+#' @noRd
+ee_html_usage_rstudio <- function(pydoc){
+  name_strong <- sprintf("<strong>%s</strong>",pydoc$qualified_name)
+  if (is.null(pydoc$signature)) {
+    return("")
+  }
+  extract_parenthesis_text <- gregexpr("(?=\\().*?(?<=\\))", pydoc$signature, perl = TRUE)
+  to_display <- name_strong %>%
+    paste0(regmatches(pydoc$signature, extract_parenthesis_text)[[1]])
+  sprintf('<h3 style="%s">Usage</h3><p style = "%s">%s</p>',ee_css_h3_rstudio(),ee_css_pre_simple(),to_display)
+}
+
 #' Create arguments - R Documentation Simple
 #' @noRd
 ee_html_arguments_simple <- function(parameters) {
   if (is.null(parameters)) {
+    return("")
+  }
+  if (length(parameters) == 0) {
     return("")
   }
   p_style <- 'display: block;margin-block-start: 1em;margin-block-end: 1em;margin-inline-start: 0px;margin-inline-end: 0px;'
@@ -160,6 +187,33 @@ ee_html_arguments_simple <- function(parameters) {
   sprintf('<h3 style="%s">Arguments</h3>%s',ee_css_h3_simple(), arguments_table)
 }
 
+#' Create arguments - R Documentation Simple
+#' @noRd
+ee_html_arguments_rstudio <- function(parameters) {
+  if (is.null(parameters)) {
+    return("")
+  }
+  if (length(parameters) == 0) {
+    return("")
+  }
+  p_style <- 'display: block;margin-block-start: 1em;margin-block-end: 1em;margin-inline-start: 0px;margin-inline-end: 0px;'
+  table_style <- 'display: table;border-collapse: separate;border-spacing: 10px;border-color: grey;'
+  td_style <- 'display: table-cell;vertical-align: inherit;font-family: sans-serif;font-size: 10pt;'
+  table_head <- sprintf('<table summary="R argblock" style="%s">',table_style)
+  table_end <- '</table>'
+  rows <- ""
+  for (index in seq_along(parameters)) {
+    parameter <- parameters[index]
+    param_name <- names(parameter)
+    row_message <- paste0(
+      sprintf('<tr valign="top"><td style="%s"><code>%s</code></td>',td_style, param_name),
+      sprintf('<td style="%s"><p style="%s">%s</p></td></tr>', td_style,p_style, parameter)
+    )
+    rows <- paste0(rows,row_message)
+  }
+  arguments_table <- paste0(table_head,rows,table_end)
+  sprintf('<h3 style="%s">Arguments</h3>%s',ee_css_h3_rstudio(), arguments_table)
+}
 
 #' Create details - R Documentation Simple
 #' @noRd
@@ -172,6 +226,18 @@ ee_html_details_simple <- function(details){
   }
 }
 
+#' Create details - R Documentation Rstudio
+#' @noRd
+ee_html_details_rstudio <- function(details){
+  if (is.null(details)) return(details)
+  if (nchar(details) < 1) {
+    details
+  } else {
+    p_style <- 'font-family: sans-serif; font-size: 10pt;'
+    sprintf('<h3 style="%s">Details</h3><p style="%s">%s<p>',ee_css_h3_rstudio(), p_style, details)
+  }
+}
+
 #' Create returns - R Documentation Simple
 #' @noRd
 ee_html_returns_simple <- function(returns) {
@@ -180,6 +246,19 @@ ee_html_returns_simple <- function(returns) {
     returns
   } else {
     sprintf('<h3 style="%s">Returns</h3><p>%s<p>',ee_css_h3_simple(), returns)
+  }
+}
+
+
+#' Create returns - R Documentation Simple
+#' @noRd
+ee_html_returns_rstudio <- function(returns) {
+  if (is.null(returns)) return(returns)
+  if (nchar(returns) < 1 ) {
+    returns
+  } else {
+    p_style <- 'font-family: sans-serif; font-size: 10pt;'
+    sprintf('<h3 style="%s">Returns</h3><p style="%s">%s<p>',ee_css_h3_rstudio(),p_style, returns)
   }
 }
 
@@ -212,10 +291,9 @@ ee_css_h3_simple <- function(){
 #' h3 tag CSS-style - Rstudio
 #' @noRd
 ee_css_h3_rstudio <- function(){
-  paste0('color: rgb(35%, 35%, 35%);font-family: sans-serif',
-         ';font-size: 1.17em;font-weight: bold;')
+  paste0('background: white;color: rgb(35%, 35%, 35%);font-family: sans-serif',
+         ';font-size: 15px;')
 }
-
 
 #' Get lhs argument from a pipe
 #' @noRd
