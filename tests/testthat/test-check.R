162dd62e9f836546298c61d64cfdca0729aca555
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2020-03-30 22:21:54 -0500

    working on travis.yml

--- tests/testthat/test-check.R
+++ tests/testthat/test-check.R
@@ -1,13 +1,17 @@
 context("rgee: ee_check test")
 library(rgee)
 ee_reattach()
+try(ee_Initialize(email = 'data.colec.fbf@gmail.com',
+              drive = TRUE,
+              gcs = TRUE))
+filename <- system.file("external/lux.shp", package = "raster")
 
 test_that("simple ee_check ",{
-  A <- ee_Initialize(
-    email = 'data.colec.fbf@gmail.com',
-    drive = TRUE,
-    gcs = TRUE
-  )
-  expect_equal(A,TRUE)
+  expect_true(ee_check_python())
+  expect_null(ee_check_rgee_python_packages())
+  expect_null(ee_check_credentials())
 })
 
+test_that("ee_check ",{
+  expect_null(ee_check())
+})
