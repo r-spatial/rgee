39fbc34f27d92becdfd666fab34558bcbc55185d
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2020-03-30 23:17:42 -0500

    two files

--- tests/testthat/test-remove.R
+++
@@ -1,9 +0,0 @@
-test_that("ee_remove_credentials",{
-  result_True <- ee_remove_credentials('test')
-  expect_true(result_True)
-})
-
-# test_that("ee_remove_credentials not-defined",{
-#   result_True <- ee_remove_credentials()
-#   expect_true(result_True)
-# })
