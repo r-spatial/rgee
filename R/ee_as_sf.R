6c78c26c6bf14c6def667c97ea489b62cbd82638
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2020-04-05 16:44:34 -0500

    ee_as_sf support download of large tables.
    when via = "getInfo"  is specified.

--- R/ee_as_sf.R
+++ R/ee_as_sf.R
@@ -1,6 +1,14 @@
 #' Convert an EE table in a sf object
 #'
 #' @param x EE table to be converted into a sf object.
+#' @param dsn Output filename. If it is missing, it will create
+#' a temporary file.
+#' @param maxFeatures If you do not set "getInfo" in via
+#' argument, ignored it. The maximum allowed number of features
+#' in the exported table. The task will fail if the exported region
+#' covers more features in the specified projection. Defaults to 5000.
+#' @param overwrite Logical. Delete data source dsn before
+#' attempting to write?.
 #' @param container Relevant when the "via" argument is
 #' defined as "drive" or "gcs". It is the name of a unique
 #' folder ('drive') or bucket ('gcs') to be exported into.
@@ -13,6 +21,7 @@
 #' @importFrom geojsonio geojson_sf
 #' @importFrom utils tail
 #' @importFrom methods as setMethod new is setGeneric
+#' @importFrom sf st_write
 #' @details
 #' The process to pass a ee$FeatureCollection, ee$Feature or ee$Geometry to
 #' your local env could be carried out by three different strategies. The
@@ -72,12 +81,17 @@
 #' }
 #' @export
 ee_as_sf <- function(x,
+                     dsn,
+                     overwrite = TRUE,
+                     maxFeatures = 5000,
                      via = "getInfo",
                      container = "rgee_backup",
                      selectors = NULL,
                      quiet = FALSE) {
-
   sp_eeobjects <- ee_get_spatial_objects('Table')
+  if (missing(dsn)) {
+    dsn <- paste0(tempfile(),".geojson")
+  }
   if (!any(class(x) %in% sp_eeobjects)) {
     stop("x is not a spatial vector Earth Engine object\n")
   }
@@ -94,9 +108,55 @@ ee_as_sf <- function(x,
   x_fc <- ee$FeatureCollection(x)
 
   if (via == "getInfo") {
-    x_fc <- x_fc$getInfo()
-    class(x_fc) <- "geo_list"
-    geojson_sf(x_fc)
+    fc_size <- 5000
+    if (maxFeatures > 5000) {
+      cat("Number of features: Calculating ...")
+      fc_size <- x_fc$size()$getInfo()
+      cat(sprintf("\rNumber of features: %s              \n", fc_size))
+      if (maxFeatures < fc_size) {
+        stop(
+          "Export too large. Specified ",
+          fc_size,
+          " features (max: ",
+          maxFeatures,
+          "). Specify higher maxFeatures value",
+          " if you intend to export a large area."
+        )
+      }
+    }
+
+    nbatch <- ceiling(fc_size / 5000)
+    if (nbatch >= 3) {
+      message(
+        "Warning: getInfo is just for small tables (max: ",
+        5000*3,
+        "). Use 'drive' or 'gcs' instead for faster download."
+      )
+    }
+
+    if (fc_size > 5000) {
+      sf_list <- list()
+      for (r_index in seq_len(nbatch)) {
+        index = r_index - 1
+        cat(
+          sprintf(
+            "Getting data from the patch: %s/%s",
+            r_index, nbatch
+          ), "\n"
+        )
+        x_eelist <- x_fc$toList(count = 5000, offset = 5000*index)
+        x_fc_batch <- ee$FeatureCollection(x_eelist)
+        sf_list[[r_index]] <- ee_fc_to_sf_getInfo(
+          x_fc = x_fc_batch,
+          overwrite = overwrite
+        )
+      }
+      x_sf_mosaic <- do.call(rbind, sf_list)
+      st_write(x_sf_mosaic, dsn, delete_dsn = overwrite, quiet = TRUE)
+      x_sf_mosaic
+    } else {
+      ee_fc_to_sf_getInfo(x_fc, dsn, overwrite)
+    }
   } else if (via == "drive") {
     # Creating name for temporal file; just for either drive or gcs
     time_format <- format(Sys.time(), "%Y-%m-%d-%H:%M:%S")
@@ -139,7 +199,12 @@ ee_as_sf <- function(x,
       stop(table_task$status()$error_message)
     }
 
-    ee_drive_to_local(table_task)
+    ee_drive_to_local(
+      table_task,
+      dsn = dsn,
+      overwrite = overwrite,
+      consider = 'all'
+    )
   } else if (via == 'gcs') {
     # Creating name for temporal file; just for either drive or gcs
     time_format <- format(Sys.time(), "%Y-%m-%d-%H:%M:%S")
@@ -182,8 +247,32 @@ ee_as_sf <- function(x,
     if (table_task$status()$state != "COMPLETED") {
       stop(table_task$status()$error_message)
     }
-    ee_gcs_to_local(table_task)
+    ee_gcs_to_local(task = table_task,dsn = dsn, overwrite = overwrite)
   } else {
     stop("type argument invalid.")
+  }
+}
+
+
+
+#' Convert a FeatureCollection to sf via getInfo
+#' @noRd
+ee_fc_to_sf_getInfo <- function(x_fc, dsn, overwrite = TRUE) {
+  x_list <- tryCatch(
+    expr = x_fc$getInfo(),
+    error = function(e) stop(
+      "Collection query aborted",
+      " after accumulating over 5000 elements.",
+      " Specify higher maxFeatures value if you",
+      " intend to export a large area via getInfo."
+    )
+  )
+  class(x_list) <- "geo_list"
+  x_sf <- geojson_sf(x_list, stringsAsFactors = FALSE)
+  if (missing(dsn)) {
+    x_sf
+  } else {
+    write_sf(x_sf, dsn, delete_dsn = overwrite, quiet = TRUE)
+    x_sf
   }
 }
