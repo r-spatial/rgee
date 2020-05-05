0a702a08546b5087e13f6f8313e2f5eaf45a3b40
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2020-05-05 16:29:07 -0500

    ee_manage_* and  ee_extract test upgrade

--- R/ee_extract.R
+++ R/ee_extract.R
@@ -141,9 +141,13 @@ ee_extract <- function(x,
   if (x_type == "ImageCollection") {
     band_names <- x$first()$bandNames()$getInfo()
     if (length(band_names) > 1) {
-      stop("ee_extract does not support ee$ImageCollection with",
-           " multiple bands"," \nEntered: ",band_names,"\nExpected: ",
-           band_names[1])
+      stop(
+        "ee_extract does not support ee$ImageCollection with",
+        " multiple bands"," \nEntered: ",
+        paste0(band_names,collapse = " "),
+        "\nExpected: ",
+        band_names[1]
+      )
     }
   } else {
     band_names <- x$bandNames()$getInfo()
--- tests/testthat/test-ee_extract.R
+++ tests/testthat/test-ee_extract.R
@@ -1,18 +1,36 @@
 context("rgee: ee_extract test")
-
 filename <- system.file("external/lux.shp", package="raster")
 
 # data --------------------------------------------------------------------
+terraclimate_raw <- ee$ImageCollection("IDAHO_EPSCOR/TERRACLIMATE")
 terraclimate <- ee$ImageCollection("IDAHO_EPSCOR/TERRACLIMATE")$
   filterDate("2000-01-01", "2001-01-01")$
   map(function(x) x$select("pr"))
 nc <- st_read(system.file("shape/nc.shp", package = "sf"), quiet = TRUE) %>%
   st_transform(4326)
 
-test_that("simple test ee_extract",{
+test_that("ee_extract ee$ImageCollection",{
   ee_nc_rain <- ee_extract(x = terraclimate,
                            y = nc,
                            fun = ee$Reducer$max(),
                            sf = TRUE)
   expect_equal(mean(ee_nc_rain$X200012),53.29)
+})
+
+test_that("ee_extract ee$Image",{
+  ee_nc_rain <- ee_extract(x = terraclimate$toBands(),
+                           y = sf_as_ee(nc),
+                           fun = ee$Reducer$max(),
+                           sf = TRUE)
+  expect_equal(mean(ee_nc_rain$X200012_pr),53.29)
+})
+
+
+
+test_that("ee_extract - error ",{
+  demo <- environment()
+  demo$name <- function() 1
+  expect_error(ee_extract(x = demo, y = nc, sf = TRUE))
+  expect_error(ee_extract(x = terraclimate, y = terraclimate, sf = TRUE))
+  expect_error(ee_extract(x = terraclimate_raw, y = nc, sf = TRUE))
 })
