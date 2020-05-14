0d354a1cfdd036a0a1e95e40cfa18db58e2049fc
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2020-05-14 06:07:00 -0500

    ee_utils file added

--- R/ee_utils.R
+++ R/ee_utils.R
@@ -71,3 +71,50 @@ ee_utils_shp_to_zip <- function(x,
   zip(zipfile = zipname, files = shp_filenames, flags = "-j -q")
   zipname
 }
+
+
+#' Wrap an R function in a Python function with the same signature.
+#' @author Yuan Tang and J.J. Allaire
+#'
+#' @description This function could wrap an R function in a Python
+#' function with the same signature. Note that the signature of the
+#' R function must not contain esoteric Python-incompatible constructs.
+#'
+#' @note \code{\link[reticulate]{py_func}} has been renamed to ee_utils_pyfunc
+#' just to maintain the rgee functions name's style. All recognition
+#' for this function must always be given to \pkg{reticulate}.
+#' @return A Python function that calls the R function `f` with the same
+#' signature.
+#' @param f An R function
+#' @examples
+#' \dontrun{
+#' library(rgee)
+#'
+#' ee_reattach() # reattach ee as a reserved word
+#' ee_Initialize()
+#'
+#' # Earth Engine List
+#' ee_SimpleList <- ee$List$sequence(0, 12)
+#' ee_NewList <- ee_SimpleList$map(
+#'   ee_utils_pyfunc(
+#'     function(x) {
+#'       ee$Number(x)$add(x)
+#'     }
+#'   )
+#' )
+#'
+#' ee_NewList$getInfo()
+#'
+#' # Earth Engine ImageCollection
+#' constant1 <- ee$Image(1)
+#' constant2 <- ee$Image(2)
+#' ee_ic <- ee$ImageCollection(c(constant2, constant1))
+#' ee_newic <- ee_ic$map(
+#'   ee_utils_pyfunc(
+#'     function(x) ee$Image(x)$add(x)
+#'   )
+#' )
+#' ee_newic$mean()$getInfo()$type
+#' }
+#' @export
+ee_utils_pyfunc <- reticulate::py_func
