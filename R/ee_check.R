020361e1abe3cd5b7a8934131df8efdb19d66378
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2020-06-19 17:37:20 -0500

    ee_check was completely renovated
    - new message display
    - Fix a bug in test GCS credentials.
    - ee_check.py is no longer necessary.

--- R/ee_check.R
+++ R/ee_check.R
@@ -1,11 +1,13 @@
 #' Interface to check Python and non-R dependencies
 #'
 #' R functions for checking Google credentials (Google Earth Engine,
-#' Google Drive and Google Cloud Storage) available in their system and the
-#' Python environment and the Third-Party Python Packages used by rgee.
+#' Google Drive and Google Cloud Storage), Python environment and the
+#' Third-Party Python Packages used by rgee.
 #'
 #' @name ee_check-tools
 #'
+#' @param user Character. User to check credentials. If it is not defined,
+#' ee_check will skip the check of credentials.
 #' @param quiet Logical. Suppress info message
 #'
 #' @importFrom reticulate py_available py_module_available py_discover_config
@@ -20,13 +22,15 @@
 #' ee_check_python()
 #' ee_check_python_packages()
 #' ee_check_credentials()
-#' ee_check() # put it all together
+#' ee_check() # put them all together
 #' }
 #' @export
-ee_check <- function() {
-  ee_check_python()
-  ee_check_python_packages()
-  ee_check_credentials()
+ee_check <- function(user = NULL, quiet = FALSE) {
+  ee_check_python(quiet = quiet)
+  ee_check_python_packages(quiet = quiet)
+  if (!is.null(user)) {
+    ee_check_credentials(quiet = quiet)
+  }
 }
 
 #' @rdname ee_check-tools
@@ -39,8 +43,10 @@ ee_check_python <- function(quiet = FALSE) {
     if (!quiet) {
       cat_line(
         blue(symbol$circle_filled),
-        blue("  Python version found: "),
-        green(
+        blue("  Python version\n"),
+        green(symbol$tick, "[Ok]"),
+        blue(
+          "",
           py_discover_config()$python,
           sprintf("v%s", py_version)
         )
@@ -48,7 +54,7 @@ ee_check_python <- function(quiet = FALSE) {
     }
   } else {
     stop(
-      "Unable to find a Python version, you will need to fix it before run ",
+      "Unable to find a Python version, you will need to fix before run ",
       "rgee::ee_Initialize(). For more details run reticulate::py_available()"
     )
   }
@@ -60,101 +66,86 @@ ee_check_python <- function(quiet = FALSE) {
 #' @family ee_check functions
 #' @export
 ee_check_python_packages <- function(quiet = FALSE) {
-  oauth_func_path <- system.file("python/ee_check.py",
-    package = "rgee"
-  )
-  ee_check_utils_exist <- ee_source_python(oauth_func_path)
+  # Necessary packages
+  if (!py_module_available("numpy") & quiet == FALSE) {
+    ee_wrong_message("numpy")
+    return(invisible(FALSE))
+  }
+  if (!py_module_available("ee") & quiet == FALSE) {
+    ee_wrong_message("earthengine-api")
+    return(invisible(FALSE))
+  }
   if (!quiet) {
     cat_line(
-      "\n",
-      blue(
-        symbol$circle_filled,
-        " Python Third-Party Libraries used in rgee: \n"
-      )
+      blue(symbol$circle_filled),
+      blue("  Python packages:\n"),
+      green(symbol$tick, "[Ok]"),
+      blue(" numpy\n"),
+      green(symbol$tick, "[Ok]"),
+      blue(" earthengine-api")
     )
   }
-  # ee
-  version_ee <- ee_utils_py_to_r(ee_check_utils_exist$ee_check_py_ee())
-  ee_cond <- is.character(version_ee)
-  if (ee_cond) {
-    if (version_ee == ee_version()) {
-      if (!quiet) {
-        cat_line(
-          green(symbol$tick, "[Ok]"),
-          blue(symbol$check, "Python Earth Engine API version "),
-          green(version_ee)
-        )
-      }
-    } else {
-      text <- paste(
-        sprintf(
-          "%s The Earth Engine Python API version %s is installed",
-          crayon::bold("NOTE:"),
-          version_ee
-        ),
-        "correctly in the system but rgee was tested using the version",
-        sprintf(
-          "%s. To avoid possible issues, we recommend install the",
-          ee_version()
-        ),
-        sprintf("version used by rgee (%s). You might use:", ee_version()),
-        sprintf(
-          "* reticulate::py_install('earthengine-api==%s')",
-          ee_version()
-        ),
-        sprintf(
-          "* pip install earthengine-api==%s (Linux and Mac0S)",
-          ee_version()
-        ),
-        sprintf(
-          "* conda install earthengine-api==%s (Windows)",
-          ee_version()
-        ),
-        "",
-        sep = "\n"
-      )
-      message(text)
-    }
-  } else {
-    if (isFALSE(quiet)) {
-      cat_line(
-        red(symbol$tick, "[X]"),
-        red(" Not installed"),
-        red(
-          symbol$check,
-          "Python Earth Engine API",
-          "(earthengine-api)"
-        )
-      )
-    }
+  ee_current_version <- ee$'__version__'
+  if (!ee_current_version == ee_version()) {
+    text <- paste(
+      sprintf(
+        "%s The Earth Engine Python API version %s is installed",
+        crayon::bold("NOTE:"),
+        ee_current_version
+      ),
+      "correctly in the system but rgee was tested using the version",
+      sprintf(
+        "%s. To avoid possible issues, we recommend install the",
+        ee_version()
+      ),
+      sprintf("version used by rgee (%s). You might use:", ee_version()),
+      "* rgee::ee_install_upgrade()",
+      sprintf(
+        "* reticulate::py_install('earthengine-api==%s')",
+        ee_version()
+      ),
+      sprintf(
+        "* pip install earthengine-api==%s (Linux and Mac0S)",
+        ee_version()
+      ),
+      sprintf(
+        "* conda install earthengine-api==%s (Linux, Mac0S, and Windows)",
+        ee_version()
+      ),
+      "",
+      sep = "\n"
+    )
+    message(text)
   }
-  if (!quiet) {
-    ee_check_py_package("pyasn1")
-    ee_check_py_package("urllib3")
-    ee_check_py_package("setuptools")
-    ee_check_py_package("oauth2client")
-    ee_check_py_package("numpy")
-    cat("\n")
-  }
+  invisible(TRUE)
 }
 
 #' @rdname ee_check-tools
 #' @family ee_check functions
 #' @export
 ee_check_credentials <- function(quiet = FALSE) {
+  # get the path of earth engine credentials
   oauth_func_path <- system.file("python/ee_utils.py", package = "rgee")
   utils_py <- ee_source_python(oauth_func_path)
   driverdir <- ee_utils_py_to_r(utils_py$ee_path())
-  ee_credentials <- sprintf("%s/credentials", driverdir)
-  drive_credentials <- list.files(driverdir, "@gmail.com", full.names = TRUE)[1]
 
+  # Exist the EE credential?
+  ee_credentials <- sprintf("%s/credentials", driverdir)
   ex_ee_cred <- file.exists(ee_credentials)
+
+  # Exist the googledrive credential?
+  drive_credentials <- list.files(driverdir, "@gmail.com", full.names = TRUE)[1]
   ex_drive_cred <- file.exists(drive_credentials)
+
+  # Exist the gcs credential?
+  gcs_credentials <- list.files(driverdir, "\\.json$", full.names = TRUE)[1]
   ex_gcs_cred <- file.exists(drive_credentials)
+
+  # Google Earth Engine credentials
   if (!quiet) {
     cat_line(
       blue(symbol$circle_filled),
-      blue("  Credentials neccesaries for rgee: \n")
+      blue("  Credentials neccesaries for rgee:")
     )
   }
 
@@ -172,6 +163,7 @@ ee_check_credentials <- function(quiet = FALSE) {
     )
   }
 
+  # Google Drive credentials
   if (ex_drive_cred) {
     if (!quiet) {
       cat_line(
@@ -181,95 +173,64 @@ ee_check_credentials <- function(quiet = FALSE) {
     }
   } else {
     if (!quiet) {
-      cat_line(
-        yellow(
-          symbol$circle_cross,
-          "Does not exist Google Drive credentials in their",
-          "system. rgee::ee_download_drive() will not work."
-        )
+      message(
+        bold("NOTE: "),
+        "This user does not present a Google Drive credential. ",
+        "The following functions will not work:\n",
+        "- ee_drive_to_local()\n",
+        "- ee_as_raster(via = \"drive\")\n",
+        "- ee_as_stars(via = \"drive\")\n",
+        "- ee_as_sf(via = \"drive\")\n",
+        "Try rgee::ee_Initialize(drive = TRUE) to fix.\n"
       )
-      message("Try rgee::ee_Initialize(drive = TRUE) to fixed.\n")
     }
   }
 
+  # GCS credentials
   if (ex_gcs_cred) {
     if (!quiet) {
       cat_line(
         green(symbol$tick, "[Ok]"),
         blue(
           symbol$check,
-          "Google Cloud Storage ",
+          "Google Cloud Storage",
           "credentials found."
         )
       )
     }
   } else {
     if (!quiet) {
-      cat_line(
-        yellow(
-          symbol$circle_cross,
-          "Does not exist Google Cloud Storage credentials in their system.",
-          'rgee::ee_download_gcs() and rgee::ee_upload(bucket=" ... ") will',
-          "not work."
-        )
+      message(
+        bold("NOTE: "),
+        "This user does not present a GCS credential. ",
+        "The following functions will not work:\n",
+        "- rgee::ee_gcs_to_local()\n",
+        "- ee_as_raster(via = \"gcs\")\n",
+        "- ee_as_stars(via = \"gcs\")\n",
+        "- ee_as_sf(via = \"gcs\")\n",
+        "- sf_as_ee(via = \"gcs_to_asset\")\n",
+        "- gcs_to_ee_image\n",
+        "- raster_as_ee\n",
+        "- stars_as_ee\n",
+        "Try rgee::ee_Initialize(gcs = TRUE) to fix.\n"
       )
-      message("Try rgee::ee_Initialize(gcs = TRUE) to fixed.\n")
     }
   }
   invisible(TRUE)
 }
 
-#' Check python packages
-#' @param rgee_package package name to install
+
+#' Wrong message when a Python package is not installed
 #' @noRd
-ee_check_py_package <- function(rgee_package, quiet = FALSE) {
-  oauth_func_path <- system.file("python/ee_check.py",
-    package = "rgee"
+ee_wrong_message <- function(packages) {
+  cat_line(
+    red(symbol$tick, "[X]"),
+    red(
+      symbol$check,
+      bold(packages)
+    ),
+    red(" not installed")
   )
-  check_py_package <- ee_source_python(oauth_func_path)
-  version_rgeepackage <- ee_utils_py_to_r(
-    eval(
-      parse(
-        text = sprintf("check_py_package$ee_check_py_%s()", rgee_package)
-      )
-    )
-  )
-  rgeepackage_is_text <- is.character(version_rgeepackage)
-  rgeepackage_is_TRUE <- isTRUE(version_rgeepackage)
-
-  if (rgeepackage_is_text) {
-    if (!quiet) {
-      cat_line(
-        green(symbol$tick),
-        green(" [Ok] "),
-        blue(
-          rgee_package, "v",
-          version_rgeepackage
-        )
-      )
-    }
-  }
-
-  if (rgeepackage_is_TRUE) {
-    if (!quiet) {
-      cat_line(
-        green(symbol$tick),
-        green(" [Ok] "),
-        blue(rgee_package)
-      )
-    }
-  }
-
-  if (isFALSE(version_rgeepackage)) {
-    stop(
-      bold(rgee_package),
-      " has not been found in this Python version, try as follow for fixed: \n",
-      sprintf(
-        "- reticulate::py_install('%s') \n",
-        rgee_package
-      ),
-      "If the installation is successful, restart to see changes."
-    )
-  }
-  invisible(TRUE)
 }
+
+
