3e9ec418dd162af4ae7a24354ad3309aecaf7e05
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2025-02-12 14:51:21 +0100

    Update argument opt_url to url, cf #380

--- R/ee_Initialize.R
+++ R/ee_Initialize.R
@@ -136,7 +136,7 @@ ee_Initialize <- function(user = NULL,
   tryCatch(expr = {
     ee$Initialize(
       credentials=credentials,
-      opt_url=opt_url,
+      url=opt_url, # changed from opt_url to url with version 1.5.2
       cloud_api_key=cloud_api_key,
       http_transport=http_transport,
       project=project
@@ -149,7 +149,7 @@ ee_Initialize <- function(user = NULL,
        )
        ee$Initialize(
          credentials=credentials,
-         opt_url=opt_url,
+         url=opt_url, # changed from opt_url to url with version 1.5.2
          cloud_api_key=cloud_api_key,
          http_transport=http_transport,
          project=project
